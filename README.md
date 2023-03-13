a9051bc4e5c2b77a7d51376fed4eb55d184fff6d
Frank Jung <frankhjung@linux.com>
2023-03-14 09:31:46 +1100

    demo script with build and requirements

--- README.md
+++ README.md
@@ -1,18 +1,58 @@
-# GitHub Copilot Demonstration for reading Stock Quotes
+# Read Stock Quotes Example
 
-A small Python program to demonstrate how to read stock quotes from [Alpha Vantage](https://www.alphavantage.co/).
+Use GitHub Copilot to write a small Python program to demonstrate how to read
+stock quotes using the [Alpha Vantage](https://www.alphavantage.co/) API.
 
 ## Method
 
-1. read ALPHAVANTAGE_API_KEY from environment variable
-2. call Alpha Vantage API to get stock quotes
-3. store stock quotes to a Pandas dataframe
-4. print to console
-5. plot stock quotes to a chart (closing price by date) using Matplotlib
+- get stock symbol from command line argument
+- read `ALPHAVANTAGE_API_KEY` from environment variable
+- call Alpha Vantage API to get stock quotes
+- store stock quotes to a Pandas dataframe
+- print dataframe to console
+- plot stock quotes to a chart (closing price / date) using Matplotlib
+
+## Example
+
+```bash
+./get_weekly_quotes.py MSFT
+               open    high     low   close       volume
+1999-11-12   84.810   90.75   84.37   89.19  270831600.0
+1999-11-19   88.250   88.50   84.37   86.00  147891900.0
+1999-11-26   89.620   93.37   88.37   91.12  121834600.0
+1999-12-03   90.120   97.12   89.50   96.12  142022200.0
+1999-12-10   95.250   97.19   91.44   93.87  115003700.0
+...             ...     ...     ...     ...          ...
+2023-02-10  257.440  276.76  254.78  263.10  196239002.0
+2023-02-17  267.640  274.97  256.00  258.06  170244679.0
+2023-02-24  254.480  256.84  248.10  249.22  105098500.0
+2023-03-03  252.460  255.62  245.61  255.29  126840033.0
+2023-03-10  256.425  260.12  247.60  248.59  117910562.0
+
+[1218 rows x 5 columns]
+```
+
+## MSFT Stock Quote
+
+![Alpha Vantage MSFT](./msft-alpha-vantage.png)
+
+Compare with Google Finance:
+
+![Google Finance MSFT](./msft-google-finance.png)
 
 ## Resources
 
-* [Alpha Vantage API](https://www.alphavantage.co/documentation/)
-* [Pandas](https://pandas.pydata.org/)
-* [Python](https://python.org)
-* [Matplotlib](https://matplotlib.org/)
+- [Alpha Vantage API](https://www.alphavantage.co/documentation/)
+- [Pandas](https://pandas.pydata.org/)
+- [Matplotlib](https://matplotlib.org/)
+- [Python](https://www.python.org/)
+
+## Other Tools
+
+- [GitHub Copilot Labs](https://githubnext.com/projects/copilot-labs) provides
+  additional Copilot features, including explain code, translate code, and write
+  unit tests
+
+- [OpenAI playground](https://platform.openai.com/playground) is a research
+  project to experiment with different pre-trained AI models developed by OpenAI
+  in natural language processing (NLP), computer vision, and other AI domains
---
+++ get_weekly_quotes.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# pylint: disable=invalid-name
+
+"""
+Get weekly quotes from Alpha Vantage API.
+"""
+
+import os
+import sys
+from typing import Any
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import requests
+
+
+def get_alphavantage_key() -> str:
+    """Read Alpha Vantage key from environment variable.
+
+    Returns:
+        Alpha Vantage API key
+    """
+    try:
+        key = os.environ["ALPHAVANTAGE_API_KEY"]
+    except KeyError:
+        print("ALPHAVANTAGE_API_KEY not found in environment variables")
+        sys.exit(1)
+    return key
+
+
+def get_weekly_quotes(key: str, symbol: str) -> dict[Any, Any]:
+    """Get weekly quotes from Alpha Vantage API
+
+    Args:
+        key: Alpha Vantage API key
+        symbol: Stock symbol
+
+    Returns:
+        A dictionary of quotes for symbol
+    """
+    url = "https://www.alphavantage.co/query"
+    params = {
+        "function": "TIME_SERIES_WEEKLY",
+        "symbol": symbol,
+        "apikey": key,
+    }
+    response = requests.get(url, params=params, timeout=5)
+    if response.status_code != 200:
+        print(f"Error: status code {response.status_code}")
+        sys.exit(1)
+    return response.json()
+
+
+def convert_to_dataframe(quotes: dict[Any, Any]) -> pd.DataFrame:
+    """Convert a list of quotes to a pandas DataFrame
+
+    Args:
+        quotes: A list of quotes to be converted
+
+    Returns:
+        A Pandas DataFrame
+    """
+    # get data
+    data = quotes["Weekly Time Series"]
+    # convert to dataframe
+    df = pd.DataFrame(data).T
+    # set index to datetime
+    df.index = pd.to_datetime(df.index)
+    # rename columns
+    df.columns = ["open", "high", "low", "close", "volume"]
+    # convert columns to floating numeric types
+    df = df.astype(float)
+    # sort by index date
+    df = df.sort_index()
+    return df
+
+
+def plot_weekly_quotes(symbol: str, df: pd.DataFrame):
+    """Plot closing price by date.
+
+    Args:
+        symbol: Stock symbol
+        df: Dataframe containing the stock history for symbol
+    """
+    df["close"].plot()
+    # label title with symbol
+    plt.title(f"{symbol} Weekly Closing Price")
+    # label axes
+    plt.xlabel("Date")
+    plt.ylabel("Closing Price (USD)")
+    plt.show()
+
+
+def main():
+    """Get weekly quotes from Alpha Vantage API.
+
+    Usage: get_weekly_quotes.py <symbol>
+
+    Args:
+        symbol: Stock symbol
+
+    Environment Variables:
+        ALPHAVANTAGE_API_KEY: Alpha Vantage API key
+    """
+    # check command line arguments
+    if len(sys.argv) != 2:
+        print("Usage: get_weekly_quotes.py <symbol>")
+        sys.exit(1)
+    # get symbol from command line
+    symbol = sys.argv[1]
+    # read alpha key from environment variable
+    key = get_alphavantage_key()
+    # get weekly quotes
+    quotes = get_weekly_quotes(key, symbol)
+    # convert to dataframe
+    df = convert_to_dataframe(quotes)
+    print(df)
+    # plot
+    plot_weekly_quotes(symbol, df)
+
+
+if __name__ == "__main__":
+    main()
